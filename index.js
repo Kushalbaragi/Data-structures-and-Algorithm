5549dfcb88784814ac4e1a84855008026e81537d
Kushal baragi <Kushalbaragi83@gmail.com>
2023-06-17 15:22:16 +0530

    code update

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-// https://jsonblob.com/api/5226571730043f8b22dadc20
-fetch('./main.json')
-.then(data=>data)
-.then(data=>{
-    console.log(data);
-    data.forEach((ele)=>{
-      let para=document.createElement('p');
-      para.innerHTML=ele.name;
-        document.getElementById('body').append(para);
-    })
-}
-    );
+// // https://jsonblob.com/api/5226571730043f8b22dadc20
+// fetch('./main.json')
+// .then(data=>data)
+// .then(data=>{
+//     console.log(data);
+//     data.forEach((ele)=>{
+//       let para=document.createElement('p');
+//       para.innerHTML=ele.name;
+//         document.getElementById('body').append(para);
+//     })
+// }
+//     );
+
+fetch('./main.json').then(data=>data.json()).then(data=>{console.log(data)})
 

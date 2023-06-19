a54577e40983a0173c7be65a76a6c4a5f2615be8
Kushal baragi <Kushalbaragi83@gmail.com>
2023-06-19 19:19:23 +0530

    code save

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// // https://jsonblob.com/api/5226571730043f8b22dadc20
-// fetch('./main.json')
-// .then(data=>data)
-// .then(data=>{
-//     console.log(data);
-//     data.forEach((ele)=>{
-//       let para=document.createElement('p');
-//       para.innerHTML=ele.name;
-//         document.getElementById('body').append(para);
-//     })
-// }
-//     );
-
-fetch('./main.json').then(data=>data.json()).then(data=>{console.log(data)})
-
+let arr=[1,2,3,4,3,4];
+let newarr=new Set(arr);
+console.log(newarr);
